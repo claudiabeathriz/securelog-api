1bc224bfa69597f14758933822d8d35b173f09ce
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-10 16:01:28 -0300

    Initial commit

---
+++ src/main/java/com/claudia/securelog_api/SecurelogApiApplication.java
@@ -0,0 +1,13 @@
+package com.claudia.securelog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurelogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurelogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudia/securelog_api/SecurelogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudia.securelog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurelogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
