8d2a2fadf6e244365a7f51cb51d8df3c6f40a9db
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-12 16:01:17 -0300

    feat: create user service

---
+++ src/main/java/com/claudia/securelog_api/services/UserService.java
@@ -0,0 +1,25 @@
+package com.claudia.securelog_api.services;
+
+import com.claudia.securelog_api.entities.User;
+import com.claudia.securelog_api.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
