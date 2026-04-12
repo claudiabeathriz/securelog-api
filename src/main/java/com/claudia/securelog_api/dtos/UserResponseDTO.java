fa5e690e66726ca8871e1651be1176810b9b7a50
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-12 20:43:52 -0300

    feat: create UserResponseDTO

---
+++ src/main/java/com/claudia/securelog_api/dtos/UserResponseDTO.java
@@ -0,0 +1,20 @@
+package com.claudia.securelog_api.dtos;
+
+public class UserResponseDTO {
+    // resposta da API
+
+    private Long id;
+    private String name;
+    private String email;
+    // campos que serão mostrados; SEM SENHA
+
+    public UserResponseDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
