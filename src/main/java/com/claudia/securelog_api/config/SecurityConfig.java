0241b17474dac7740d97fdb78bfd6ea0eb95e6a0
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-13 12:25:52 -0300

    fix: permit all access csrf

--- src/main/java/com/claudia/securelog_api/config/SecurityConfig.java
+++ src/main/java/com/claudia/securelog_api/config/SecurityConfig.java
@@ -2,10 +2,23 @@ package com.claudia.securelog_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // desabilita CSRF (para testes com Postman)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // libera todas as rotas
+                );
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
