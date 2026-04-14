221bd4a5905e489adc01b6ab9e21fbc5f9e91d8c
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-04-13 21:46:51 -0300

    fix: corret login method

--- src/main/java/com/claudia/securelog_api/controllers/UserController.java
+++ src/main/java/com/claudia/securelog_api/controllers/UserController.java
@@ -16,11 +16,9 @@ import java.util.stream.Collectors;
 public class UserController {
 
     private final UserService userService;
-    // traz a logica da aplicação
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    // injeção de dependencia
 
     @PostMapping
     public UserResponseDTO create(@RequestBody @Valid UserRequestDTO dto) {
@@ -33,7 +31,6 @@ public class UserController {
         );
 
         User savedUser = userService.create(user);
-        // chama lógica do service
 
         return new UserResponseDTO(
                 savedUser.getId(),
@@ -46,9 +43,7 @@ public class UserController {
     @GetMapping
     public List<UserResponseDTO> findAll() {
         return userService.findAll()
-                // busca todos os users
                 .stream()
-                // transforma lista em stream
                 .map(user -> new UserResponseDTO(
                         // converte cada user em DTO
                         user.getId(),
@@ -56,19 +51,15 @@ public class UserController {
                         user.getEmail()
                 ))
                 .collect(Collectors.toList());
-                // volta pra lista
     }
 
     @GetMapping("/{id}")
     public UserResponseDTO findById(@PathVariable Long id) {
-        // pega o ID da URL
         User user = userService.findById(id);
-        // busca no banco (service pega Optional<User> user = userRepository.findById(id);)
         return new UserResponseDTO(
                 user.getId(),
                 user.getName(),
                 user.getEmail()
-                // retorna sem senha
         );
     }
 }
--- src/main/java/com/claudia/securelog_api/services/UserService.java
+++ src/main/java/com/claudia/securelog_api/services/UserService.java
@@ -9,11 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-// annotation para informar que é um componente de negócio
 public class UserService {
 
     private final UserRepository userRepository;
-    // dependência do repo; não muda
 
     private final BCryptPasswordEncoder passwordEncoder;
 
@@ -22,45 +20,35 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
-    // injeção de dependência + senha criptografada
 
     public User create(User user) {
+        userRepository.findByEmail(user.getEmail())
+                .ifPresent(u -> {
+                    throw new RuntimeException("Email already exists");
+                });
+
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
+
         return userRepository.save(user);
     }
-    // no sql => INSERT INTO users (...)
 
     public List<User> findAll() {
         return userRepository.findAll();
     }
-    // no sql => SELECT * FROM users;
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        // optional porque o user pode existir ou nao
         return user.orElseThrow(() -> new RuntimeException("User not found"));
-        // tratamento de exceção evita que retorne null = boa prática
     }
-    // no sql => SELECT * FROM users WHERE id = ?
-    // ou
-    // SELECT u.id, u.name, u.email, u.password
-    //FROM users u
-    //WHERE u.id = ?;
 
     public User findByEmail(String email) {
         Optional<User> user = userRepository.findByEmail(email);
-        // optional porque o user pode existir ou nao
         return user.orElseThrow(() -> new RuntimeException("User not found"));
-        // tratamento de exceção evita que retorne null = boa prática
     }
 
     public User login(String email, String password) {
         User user = findByEmail(email);
-
-        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new RuntimeException("Email already exists");
-        }
 
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
